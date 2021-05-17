0768678b11c9aba6d691e45ebf47bd34c8c95fa2
amascii <manguin98@gmail.com>
2021-05-17 18:22:36 -0500

    extending templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+
+# urlpatterns = [
+#     path('', views.post_list, name='post_list'),
+#     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+# ]
